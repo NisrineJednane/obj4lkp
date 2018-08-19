086c5556c3ddf8bac9328b433e26975983ceed83
Nisrine Jednane <34342007+NisrineJednane@users.noreply.github.com>
2018-08-19 13:08:36 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function phoneticLookup(val) {
+ var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  };
+  var result = lookup[val];
+  return result;
+}
